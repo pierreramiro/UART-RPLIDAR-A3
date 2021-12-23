4380e97994b860115bc0800a20aad8f0c0454d08
Pierre Pérez <95595307+pierreramiro@users.noreply.github.com>
2021-12-23 17:04:06 -0500

    Se añadio funcion setMotorDutyCycle()
    Con esta función tenemos control de la velocidad del motor.

--- STMCUBEIDE/main.c
+++ STMCUBEIDE/main.c
@@ -18,8 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "string.h"
-#include <math.h>
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -61,6 +60,8 @@ UART_HandleTypeDef huart1;
 DMA_HandleTypeDef hdma_usart1_rx;
 DMA_HandleTypeDef hdma_usart1_tx;
 
+TIM_HandleTypeDef htim1;
+
 /* USER CODE BEGIN PV */
 __IO ITStatus UartReady = RESET;
 __IO ITStatus UserButtonStatus = 0;  /* set to 1 after User Button interrupt  */
@@ -74,6 +75,7 @@ static void MX_GPIO_Init(void);
 static void MX_LPUART1_UART_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle){
@@ -109,7 +111,6 @@ uint8_t BinToAsc(uint8_t BinValue)
     if(BinValue > 9) BinValue += 7;
     return(BinValue + '0');
 }
-
 // Reverses a string 'str' of length 'len'
 void reverse(uint8_t* str, int len)
 {
@@ -187,6 +188,7 @@ void printf_data(uint8_t* pkt){
 	uint16_t angle,distance;
 	float result;
 	uint8_t ascii_chars[30];
+
 	/****Decodificamos el Quality****/
 	HAL_UART_Transmit(&hlpuart1,(uint8_t*)"Quality: ", 9, 30);
 	//Realizamos el desplazamiento y el bitmasking
@@ -215,6 +217,19 @@ void printf_data(uint8_t* pkt){
 	HAL_UART_Transmit(&hlpuart1,ascii_chars, 9, 30);
 	//Imprimos una nueva línea
 	HAL_UART_Transmit(&hlpuart1, (uint8_t*)"\n\r", 2, 100);
+}
+
+void setMotorDutyCycle(float duty){
+	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_2);
+	HAL_Delay(3);
+	if (duty!=0){
+		//El ARR tiene como valor máximo 6799
+		TIM1->CCR2 = duty*68;
+		HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+		HAL_Delay(3);
+
+	}
+
 }
 
 void SEND_STOP_REQUEST(){
@@ -284,6 +299,7 @@ void SEND_SCAN_REQUEST(){
 	while(1){
 		HAL_UART_Receive(&huart1,rx_buffer,5,1);//(uart handle,tx pointer,size, timeout)
 		printf_pkt(rx_buffer,5);
+		printf_data(rx_buffer);
 	}
 
 
@@ -469,24 +485,34 @@ int main(void)
   MX_LPUART1_UART_Init();
   MX_DMA_Init();
   MX_USART1_UART_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  uint8_t eraser_reg[70];
   /* USER CODE END 2 */
-  SEND_STOP_REQUEST();
+
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+	  setMotorDutyCycle(25);
 	  //HAL_UART_Receive(&huart1, eraser_reg,70 , 4);
 	  //Esperamos a que se presione el Boton
-	  HAL_UART_Transmit(&hlpuart1, (uint8_t*)"Comando linea:\n\r", 16, 300);
+	  //HAL_UART_Transmit(&hlpuart1, (uint8_t*)"Comando linea:\n\r", 16, 300);
 	  while(!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
 	  //Esperamos que se suelte
 	  while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
 	  /*Enviamos el comando GET_HEALTH*/
-	  SEND_SCAN_REQUEST();
+	  //SEND_SCAN_REQUEST();
+	  setMotorDutyCycle(50);
+	  while(!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
+	  while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
 
-
+	  setMotorDutyCycle(75);
+	  while(!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
+	  while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
+	  setMotorDutyCycle(100);
+	  while(!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
+	  while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13));
 	  //SEND_GET_HEALTH_REQUEST();
 	  /*Verificamos si estamos en Protection STOP*/
 
@@ -667,6 +693,88 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 6799;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.BreakFilter = 0;
+  sBreakDeadTimeConfig.BreakAFMode = TIM_BREAK_AFMODE_INPUT;
+  sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+  sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+  sBreakDeadTimeConfig.Break2Filter = 0;
+  sBreakDeadTimeConfig.Break2AFMode = TIM_BREAK_AFMODE_INPUT;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
   * Enable DMA controller clock
   */
 static void MX_DMA_Init(void)
@@ -702,7 +810,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -710,12 +818,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LD2_Pin PA9 */
-  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_9;
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
